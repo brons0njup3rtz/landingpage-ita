fb5fa7648bdd9a0cb8f7f7a4ec62aef72cb9b02a
brons0njup3rtz <bronsonjuperto@gmail.com>
2026-05-10 22:59:19 +0800

    feat: initialize landing page with layout, global styles, and tracking scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '979286171416495');
+            fbq('init', '1297474259134397');
             fbq('track', 'Contact');
           `}
         </Script>
@@ -59,7 +59,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=979286171416495&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1297474259134397&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
